28bebc579a4cef05d2466a027f7bcf5d1628cb38
BuminMacintosh <bumin_pol-software@yahoo.co.jp>
2013-12-06 17:11:51 +0900

    Target Information WPF版 初期バージョン

---
+++ TargetInformation/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace FFXIV.Tools.TargetInformation
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // FFXIVLIBに使用する言語を指定する
+            Constants.ResourceParser.RESOURCES_LANGUAGE = "ja";
+
+            // タイマーの設定（デフォルト100ミリ秒でターゲット情報取得）
+            this.timer = new DispatcherTimer(DispatcherPriority.Normal);
+            this.timer.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            this.timer.Tick += this.UpdateInformation_Tick;
+            this.timer.Start();
+        }
+
+        private void Timer_Start(object sender, MouseEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("MainWindow_MouseLeave");
+            this.timer.Start();
+        }
+
+        private void Timer_Stop(object sender, MouseEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("MainWindow_MouseEnter");
+            this.timer.Stop();
+        }
+
+        private void UpdateInformation_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                var ffxiv = new ffxivlib.FFXIVLIB();
+                this.DataContext = new Model.TargetInformationModel(ffxiv.GetCurrentTarget(),
+                                                                    ffxiv.GetPartyMemberInfo(0));
+            }
+            catch (Exception ex)
+            {
+                this.DataContext = new Model.TargetInformationModel(ex);
+            }
+        }
+
+        private void WindowBackground_DragDelta(object sender, System.Windows.Controls.Primitives.DragDeltaEventArgs e)
+        {
+            this.Left += e.HorizontalChange;
+            this.Top += e.VerticalChange;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ TargetInformation/Model/TargetInformationModel.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FFXIV.Tools.TargetInformation.Model
+{
+    /// <summary>
+    /// ターゲットインフォメーションアプリのモデルクラス
+    /// </summary>
+    public class TargetInformationModel
+    {
+        private Exception Ex { get; set; }
+        private ffxivlib.Entity Target { get; set; }
+        private ffxivlib.PartyMember Me { get; set; }
+
+        /// <summary>
+        /// デフォルトコンストラクタ
+        /// </summary>
+        /// <param name="target">ターゲットを表すFFXIVのオブジェクト</param>
+        /// <param name="me">自分自身を表すFFXIVのオブジェクト</param>
+        public TargetInformationModel(ffxivlib.Entity target, ffxivlib.PartyMember me)
+        {
+            this.Target = target;
+            this.Me = me;
+            this.Ex = null;
+        }
+
+        /// <summary>
+        /// 例外が発生したとき用のコンストラクタ
+        /// </summary>
+        /// <param name="ex"></param>
+        public TargetInformationModel(Exception ex)
+        {
+            this.Target = null;
+            this.Me = null;
+            this.Ex = ex;
+        }
+
+        /// <summary>
+        /// ターゲットの名前
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                if (null != this.Ex) return this.Ex.Message;
+                return this.Target.Structure.Name;
+            }
+        }
+
+        /// <summary>
+        /// ターゲットの現在HP
+        /// </summary>
+        public string CurrentHP
+        {
+            get
+            {
+                if (null != this.Ex) return string.Empty;
+                return this.Target.Structure.CurrentHP.ToString();
+            }
+        }
+
+        /// <summary>
+        /// ターゲットの最大HP
+        /// </summary>
+        public string MaxHP
+        {
+            get
+            {
+                if (null != this.Ex) return string.Empty;
+                return this.Target.Structure.MaxHP.ToString();
+            }
+        }
+
+        /// <summary>
+        /// ターゲットのHP/MaxHPの百分率
+        /// </summary>
+        public string RateHP
+        {
+            get
+            {
+                if (null != this.Ex) return string.Empty;
+                return string.Format(
+                    "{2:0.00}",
+                    (double)this.Target.Structure.CurrentHP / (double)this.Target.Structure.MaxHP * 100
+                );
+            }
+        }
+
+        /// <summary>
+        /// ターゲットと自分との距離
+        /// </summary>
+        public string Distance
+        {
+            get
+            {
+                if (null != this.Ex) return string.Empty;
+                return string.Format(
+                    "{0:0.00}",
+                    (double)Math.Sqrt(
+                        Math.Pow(this.Me.Structure.X - this.Target.Structure.X, 2)
+                        + Math.Pow(this.Me.Structure.Y - this.Target.Structure.Y, 2)
+                        + Math.Pow(this.Me.Structure.Z - this.Target.Structure.Z, 2)
+                    )
+                );
+            }
+        }
+    }
+}
